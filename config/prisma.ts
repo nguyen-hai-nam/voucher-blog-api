e90a2fb4a22362d8b363f9e3a8da8cec240d0d42
namtotbung <nam.nn265@gmail.com>
2023-09-28 12:40:34 +0700

    Config log function in prisma

--- config/prisma.ts
+++ config/prisma.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+	log: ['info', 'warn', 'error']
+});
 
 export default prisma;
