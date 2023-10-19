c5b53cc74fbcaf607f91bb9083e5b481a5471c5c
nguyen-hai-nam <nam.nn265@gmail.com>
2023-10-19 10:30:15 +0700

    Create user service

---
+++ services/user.service.ts
@@ -0,0 +1,49 @@
+import { Prisma } from '@prisma/client';
+
+import prisma from '../config/prisma';
+
+const countUsers = async (query: Prisma.UserWhereInput) => {
+	const count = await prisma.user.count({
+		where: { ...query }
+	});
+	return count;
+};
+
+const getAllUsers = async (query: Prisma.UserWhereInput) => {
+	const users = await prisma.user.findMany({
+		where: { ...query }
+	});
+	return users;
+};
+
+const getUserById = async (id: string) => {
+	const user = await prisma.user.findUnique({
+		where: { id }
+	});
+	return user;
+};
+
+const updateUserById = async (id: string, updateData: Prisma.UserUpdateInput) => {
+	const updatedUserId = await prisma.user.update({
+		where: { id },
+		data: updateData,
+		select: { id: true }
+	});
+	return updatedUserId;
+};
+
+const deleteUserById = async (id: string) => {
+	const deletedUserId = await prisma.user.delete({
+		where: { id },
+		select: { id: true }
+	});
+	return deletedUserId;
+};
+
+export default {
+	countUsers,
+	getAllUsers,
+	getUserById,
+	updateUserById,
+	deleteUserById
+};
