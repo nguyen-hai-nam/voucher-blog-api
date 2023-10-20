b9c6f9b3cfa274a21777b77b55458d154164515c
nguyen-hai-nam <nam.nn265@gmail.com>
2023-10-20 10:29:21 +0700

    Create error handler

--- config/express.ts
+++ config/express.ts
@@ -2,7 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
+
 import routes from '../routes/index.route';
+import errorHandler from '../middlewares/errorHandler.middleware';
 
 const app = express();
 
@@ -12,5 +14,6 @@ app.use(bodyParser.urlencoded({ extended: true, limit: '100kb' }));
 app.use(bodyParser.json({ limit: '5mb' }));
 
 app.use('/', routes);
+app.use('/', errorHandler);
 
 export default app;
