0e4e5a2932fa5685be5ac6fbcfb0479f71a7fb9d
nguyen-hai-nam <nam.nn265@gmail.com>
2023-10-20 11:14:47 +0700

    Utilize error handler and service in auth controller

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -1,76 +1,32 @@
 import { RequestHandler } from 'express';
 import { User } from '@prisma/client';
 
-import prisma from '../config/prisma';
 import { Payload } from '../interfaces/payload';
-import { generateAccessToken } from '../helpers/auth/jwt';
-import { comparePasswords, hashPassword } from '../helpers/auth/password';
-import { saltRounds } from '../constants/auth.constant';
-import { isEmail, isVietnamPhoneNumber } from '../helpers/validators';
+import AuthService from '../services/auth.service';
 
 const register: RequestHandler<
 	{},
 	{ message: string; token?: string; data?: User; error?: unknown },
 	{ email?: string; phoneNumber?: string; password: string },
 	{}
-> = async (req, res) => {
+> = async (req, res, next) => {
 	const { email, phoneNumber, password } = req.body;
 	if (!((email || phoneNumber) && password)) {
-		return res.status(400).json({ message: 'Missing information', error: 'Missing information' });
-	} else if (email && !isEmail(email)) {
-		return res.status(400).json({ message: 'Invalid email', error: 'Invalid email' });
-	} else if (phoneNumber && !isVietnamPhoneNumber(phoneNumber)) {
-		return res.status(400).json({ message: 'Invalid phone number', error: 'Invalid phone number' });
+		return res.status(400).json({ message: 'Missing credentials' });
 	}
 	try {
-		let existingEmail;
-		let existingPhoneNumber;
+		let token;
 		if (email) {
-			existingEmail = await prisma.userEmail.findUnique({
-				where: { id: email }
-			});
-			if (existingEmail) {
-				return res.status(400).json({ message: 'Email is already in use' });
-			}
+			token = await AuthService.registerWithEmail(email, password);
+		} else if (phoneNumber) {
+			token = await AuthService.registerWithPhoneNumber(phoneNumber, password);
 		}
-		if (phoneNumber) {
-			existingPhoneNumber = await prisma.userPhoneNumber.findUnique({
-				where: { id: phoneNumber }
-			});
-			if (existingPhoneNumber) {
-				return res.status(400).json({ message: 'Phone number is already in use' });
-			}
-		}
-		const hashedPassword = await hashPassword(password, saltRounds);
-		const newUser = await prisma.user.create({
-			data: {
-				password: hashedPassword
-			}
+		return res.status(201).json({
+			message: 'Success',
+			token
 		});
-		if (email) {
-			await prisma.userEmail.create({
-				data: {
-					id: email,
-					user_id: newUser.id
-				}
-			});
-		}
-		if (phoneNumber) {
-			await prisma.userPhoneNumber.create({
-				data: {
-					id: phoneNumber,
-					user_id: newUser.id
-				}
-			});
-		}
-		return res
-			.status(201)
-			.json({ message: 'Register successfully', token: generateAccessToken(newUser), data: newUser });
 	} catch (error) {
-		return res.status(500).json({
-			message: 'An error occurred while registering',
-			error
-		});
+		next(error);
 	}
 };
 
@@ -79,53 +35,24 @@ const login: RequestHandler<
 	{ message: string; token?: string; error?: unknown },
 	{ email?: string; phoneNumber?: string; password: string },
 	{}
-> = async (req, res) => {
+> = async (req, res, next) => {
 	const { email, phoneNumber, password } = req.body;
 	if (!((email || phoneNumber) && password)) {
-		return res.status(400).json({ message: 'Missing information', error: 'Missing information' });
+		return res.status(400).json({ message: 'Missing credentials' });
 	}
 	try {
-		let existingEmail;
-		let existingPhoneNumber;
-		let user;
+		let token;
 		if (email) {
-			existingEmail = await prisma.userEmail.findUnique({
-				where: { id: email }
-			});
+			token = await AuthService.loginWithEmail(email, password);
 		} else if (phoneNumber) {
-			existingPhoneNumber = await prisma.userPhoneNumber.findUnique({
-				where: { id: phoneNumber }
-			});
+			token = await AuthService.loginWithPhoneNumber(phoneNumber, password);
 		}
-		if (!(existingEmail || existingPhoneNumber)) {
-			return res.status(401).json({ message: 'Incorrect information', error: 'Incorrect information' });
-		}
-		if (existingEmail) {
-			user = await prisma.user.findUnique({
-				where: { id: existingEmail.user_id }
-			});
-		} else if (existingPhoneNumber) {
-			user = await prisma.user.findUnique({
-				where: { id: existingPhoneNumber.user_id }
-			});
-		}
-		if (!user) {
-			return res.status(401).json({ message: 'Incorrect information', error: 'Incorrect information' });
-		}
-		const isValid = await comparePasswords(password, user.password);
-		if (!isValid) {
-			return res.status(401).json({ message: 'Incorrect information', error: 'Incorrect information' });
-		}
-		const token = generateAccessToken(user);
 		return res.status(200).json({
-			message: 'Log in successfully',
+			message: 'Success',
 			token
 		});
 	} catch (error) {
-		return res.status(500).json({
-			message: 'An error occurred while logging in',
-			error
-		});
+		next(error);
 	}
 };
 
@@ -135,34 +62,13 @@ const changePassword: RequestHandler<
 	{ message: string; error?: unknown },
 	{ payload: Payload; oldPassword: string; newPassword: string },
 	{}
-> = async (req, res) => {
+> = async (req, res, next) => {
 	const { payload, oldPassword, newPassword } = req.body;
 	try {
-		const user = await prisma.user.findUnique({
-			where: { id: payload.id }
-		});
-		if (!user) {
-			return res.status(404).json({ message: 'User not found', error: 'User not found' });
-		}
-		const isValid = await comparePasswords(oldPassword, user.password);
-		if (!isValid) {
-			return res.status(400).json({ message: 'Incorrect password', error: 'Incorrect password' });
-		}
-		const hashedPassword = await hashPassword(newPassword, saltRounds);
-		await prisma.user.update({
-			where: {
-				id: payload.id
-			},
-			data: {
-				password: hashedPassword
-			}
-		});
-		return res.status(200).json({ message: 'Change password successfully' });
+		await AuthService.changePassword(payload.id, oldPassword, newPassword);
+		return res.status(200).json({ message: 'Success' });
 	} catch (error) {
-		return res.status(500).json({
-			message: 'An error occurred while changing password',
-			error
-		});
+		next(error);
 	}
 };
 
