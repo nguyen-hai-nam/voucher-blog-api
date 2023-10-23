33abb4208d21dcc5b98b9eb8e18615efdc6dc50f
nguyen-hai-nam <nam.nn265@gmail.com>
2023-10-23 15:12:52 +0700

    Refactor business routes

--- routes/business.route.ts
+++ routes/business.route.ts
@@ -11,6 +11,10 @@ router.post('/', isAuth, BusinessController.createBusiness);
 router.get('/:id', isAuth, BusinessController.getBusinessById);
 router.patch('/:id', isAuth, BusinessController.updateBusinessById);
 router.delete('/:id', isAuth, BusinessController.deleteBusinessById);
+router.post('/:business_id/address', isAuth, BusinessController.createBusinessAddress);
+router.patch('/:business_id/address/:address_id', isAuth, BusinessController.updateBusinessAddressById);
+router.post('/:business_id/timetable', isAuth, BusinessController.createBusinessTimetable);
+router.patch('/:business_id/timetable/:timetable_id', isAuth, BusinessController.updateBusinessTimetableById);
 router.post('/:id/follow', isAuth, BusinessController.followBusinessById);
 router.post('/:id/unfollow', isAuth, BusinessController.unfollowBusinessById);
 
