6b80fed49d65eb27756e07e9e7deaf98c7bb80d1
nguyen-hai-nam <nam.nn265@gmail.com>
2023-10-27 11:39:46 +0700

    Add business suggestion route

--- routes/feed.route.ts
+++ routes/feed.route.ts
@@ -6,6 +6,6 @@ import { isAuth } from '../middlewares/auth.middleware';
 const router = express.Router();
 
 router.get('/:user_id/:address_id', isAuth, FeedController.getNewsfeed);
-// router.get('/suggest', isAuth, FeedController.getUnfollowedNearbyBusinesses);
+router.get('/:user_id/:address_id/suggestion', isAuth, FeedController.getBusinessSuggestion);
 
 export default router;
